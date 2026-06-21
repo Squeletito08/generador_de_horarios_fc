59c18ba6aefffa9876aaa68c77ec214745b4d017
Gael Antonio Morales Chaparro <gaelchaparro@ciencias.unam.mx>
2026-06-21 02:41:04 -0600

    Bug fix para pasar correctamente el máximo número de optativas

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from filters.modelos import FiltrosGruposBuilder
 from filters.opciones import opciones_programa, genera_lista_modalidades
 from filters.filtrador_grupos import filtra_grupos
 
+from utils.constantes import OPTATIVAS_FALTANTES
 from utils.output import imprime_horarios, imprime_materias
 
 from utils.horarios import crea_horarios 
@@ -25,12 +26,12 @@ def main():
 
     if args.materias:
         materias_a_usar = {id: materia for id, materia in materias_dict.items() if id in args.materias}
-        horarios = crea_horarios(materias_a_usar, filtros)
+        horarios = crea_horarios(materias_a_usar, filtros, numero_maximo_optativas)
     else:
         materias_faltantes = obten_materias_faltantes(materias_dict)
         # no_optativas = filtra_materias_por_bloque(materias_faltantes, [BloqueMaterias.OPTATIVA], incluye=False)
         imprime_materias(materias_faltantes)
-        horarios = crea_horarios(materias_faltantes, filtros)
+        horarios = crea_horarios(materias_faltantes, filtros, OPTATIVAS_FALTANTES)
 
     imprime_horarios(horarios)
 
--- utils/horarios.py
+++ utils/horarios.py
@@ -5,14 +5,14 @@ from filters.filtrador_grupos import filtra_grupos
 from generator import genera_todos_los_horarios_validos
 from .constantes import OPTATIVAS_FALTANTES
 
-def crea_horarios(materias, filtros):
+def crea_horarios(materias, filtros, numero_maximo_optativas):
     grupos = []
 
     for _, materia in materias.items():
         grupos.extend(obtener_horarios_de_materia(SEMESTRE, CarrerasFacultadDeCiencias.CIENCIAS_DE_LA_COMPUTACION.id, materia))
     grupos = filtra_grupos(filtros, grupos)
 
-    return genera_todos_los_horarios_validos(grupos, OPTATIVAS_FALTANTES)
+    return genera_todos_los_horarios_validos(grupos, numero_maximo_optativas)
 
 
 
